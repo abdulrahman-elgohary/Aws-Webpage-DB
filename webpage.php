2a6fce55f4236f9053bd793e987f371611d434a2
Elgohary <151883140+abdulrahman-elgohary@users.noreply.github.com>
2024-10-12 19:30:17 +0300

    Update webpage.php

--- webpage.php
+++ webpage.php
@@ -1,7 +1,7 @@
 <?php
-$db_host = 'your-rds-endpoint.rds.amazonaws.com';  // RDS endpoint
+$db_host = 'db-rds.c7giwasc6mp0.us-west-1.rds.amazonaws.com';  // RDS endpoint
 $db_user = 'admin';  // RDS master username
-$db_password = 'yourpassword';  // RDS password
+$db_password = '12345678';  // RDS password
 $db_name = 'myappdb';  // Database name
 
 $connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
